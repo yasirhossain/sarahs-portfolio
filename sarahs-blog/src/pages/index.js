24e22e32f4b2fd31b0fce5ccbd7f32f8605b5d74
Yasir <yhossain@gmail.com>
2023-02-05 23:52:34 -0800

    migrated meta tags

--- sarahs-blog/src/pages/index.js
+++ sarahs-blog/src/pages/index.js
@@ -11,9 +11,15 @@ export default function Home() {
   return (
     <div className={styles.appContainer}>
       <Head>
-        <title>Sarah's Portfolio</title>
-        <meta name="description" content="Conceptualize and execute creative and compelling digital editorial and branded content." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Sarahs Portfolio</title>
+        <meta name="description" content="Sarah Hashim-Waris is a video producer and creative director specializing in entertainment, beauty and news content. Check out examples of my work in directing, writing, producing, and researching." />
+        <meta name="keywords" content="video producer, entertainment, beauty, news, directing, writing, producing, researching" />
+        <meta name="author" content="Sarah Hashim-Waris" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="Sarah Hashim-Waris - Video Producer Portfolio" />
+        <meta property="og:description" content="Sarah Hashim-Waris is a video producer and creative director specializing in entertainment, beauty and news content. Check out examples of my work in directing, writing, producing, and researching." />
+        <meta property="og:url" content="https://www.sarahhashimwaris.com" />
+        <meta property="og:type" content="website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavProvider>
