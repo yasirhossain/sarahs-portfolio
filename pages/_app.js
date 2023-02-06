3df09c6d8169106bf21a5959c5d88a2be1d7b026
Yasir <yhossain@gmail.com>
2023-02-05 23:43:57 -0800

    added SEO and nav

--- pages/_app.js
+++ pages/_app.js
@@ -23,6 +23,14 @@ export default function Nextra({ Component, pageProps }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;500;700;800;900&family=Reenie+Beanie&display=swap" rel="stylesheet">
         <link rel="canonical" href="https://fonts.google.com/specimen/Prata">
+        <meta name="description" content="Sarah Hashim-Waris is a video producer and creative director specializing in entertainment, beauty and news content. Check out examples of my work in directing, writing, producing, and researching.">
+        <meta name="keywords" content="video producer, entertainment, beauty, news, directing, writing, producing, researching">
+        <meta name="author" content="Sarah Hashim-Waris">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta property="og:title" content="Sarah Hashim-Waris - Video Producer Portfolio">
+        <meta property="og:description" content="Sarah Hashim-Waris is a video producer and creative director specializing in entertainment, beauty and news content. Check out examples of my work in directing, writing, producing, and researching.">
+        <meta property="og:url" content="https://www.sarahhashimwaris.com">
+        <meta property="og:type" content="website">
       </Head>
       <Component {...pageProps} />
     </>
--- sarahs-blog/src/components/Nav.js
+++ sarahs-blog/src/components/Nav.js
@@ -1,61 +1,80 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
+import MenuIcon from '@mui/icons-material/Menu'
 import { NavContext } from '@/context/NavContext'
 import styles from '@/styles/Nav.module.scss'
 
-const navLinks = [
-    { navLinkId: 'Home', scrollToId: 'homeContainer' },
-    { navLinkId: 'Portfolio', scrollToId: 'portfolioContainer' },
-    // { navLinkId: 'Contact', scrollToId: 'contactContainer' },
-];
+const Nav = () => {
+    const [checked, setChecked] = useState(false);
 
-const NavLink = ({ navLinkId, scrollToId }) => {
-    const { activeNavLinkId, setActiveNavLinkId } = useContext(NavContext);
+    const navLinks = [
+        { navLinkId: 'Home', scrollToId: 'homeContainer' },
+        { navLinkId: 'Portfolio', scrollToId: 'portfolioContainer' },
+        // { navLinkId: 'Contact', scrollToId: 'contactContainer' },
+    ];
+    
+    const NavLink = ({ navLinkId, scrollToId }) => {
+        const { activeNavLinkId, setActiveNavLinkId } = useContext(NavContext);
+    
+        const handleClick = () => {
+            setActiveNavLinkId(navLinkId);
+            handleChange();
+            document.getElementById(scrollToId).scrollIntoView({ behavior: 'smooth' });
+        };
+    
+        return (
+            <li
+                id={navLinkId}
+                className={`${activeNavLinkId === navLinkId ? styles.activeClass : ''}`}
+                onClick={handleClick}
+            >
+                {navLinkId}
+            </li>
+        );
+    };
+    
 
-    const handleClick = () => {
-        setActiveNavLinkId(navLinkId);
-        document.getElementById(scrollToId).scrollIntoView({ behavior: 'smooth' });
+    const handleChange = () => {
+        setChecked(!checked);
     };
 
     return (
-        <span
-            id={navLinkId}
-            className={activeNavLinkId === navLinkId ? 'activeClass' : ''}
-            onClick={handleClick}
-        >
-            {navLinkId}
-        </span>
-    );
-};
-
-export default function Nav() {
-    return (
-        <nav className={styles.nav}>
-            {navLinks.map(({ navLinkId, scrollToId }, idx) => (
-                <NavLink key={idx} navLinkId={navLinkId} scrollToId={scrollToId} />
-            ))}
-            <div className={styles.grid}>
-			<a
-				href="https://www.linkedin.com/in/shashimwaris/"
-				className={styles.card}
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<h2 className={styles.className}>
-				LinkedIn <span>-&gt;</span>
-				</h2>
-			</a>
-
-			<a
-				href="mailto:sarahhashimwaris@gmail.com"
-				className={styles.card}
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<h2 className={styles.className}>
-				Email <span>-&gt;</span>
-				</h2>
-			</a>
-			</div>
+        <nav className={`${styles.wrapper} ` + `${checked ? styles.active : ''}`}>
+            <input type="checkbox" id="active" onChange={handleChange} />
+            <label for="active" className={styles.menuBtn}><MenuIcon className={styles.menuIcon} /></label>
+            <ul>
+                {navLinks.map(({ navLinkId, scrollToId }, idx) => (
+                    <NavLink key={idx} navLinkId={navLinkId} scrollToId={scrollToId} />
+                ))}
+                <li onClick={handleChange}>
+                    <a
+                        href="https://www.linkedin.com/in/shashimwaris/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        LinkedIn
+                    </a>
+                </li>
+                <li onClick={handleChange}>
+                    <a
+                    href="https://sarahhashimwaris.com/shw-resume.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Resume
+                    </a>
+                </li>
+                <li onClick={handleChange}>
+                    <a
+                    href="mailto:sarahhashimwaris@gmail.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Email 
+                    </a>
+                </li>
+            </ul>
         </nav>
     )
-}
+};
+
+export default Nav;
